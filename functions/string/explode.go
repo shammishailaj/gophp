f43c629f8485ce4ef51a21ef154bb604d3836e6c
Shammi Shailaj <shammishailaj@gmail.com>
2018-09-27 17:53:55 +0530

    adding files from new structure

---
+++ functions/string/explode.go
@@ -0,0 +1,9 @@
+package string
+
+func Explode(delimiter, text string) []string {
+	if len(delimiter) > len(text) {
+		return strings.Split(delimiter, text)
+	} else {
+		return strings.Split(text, delimiter)
+	}
+}
