8d48c9880f8f3d9ac1080b2a164a5090275b96dc
Shammi Shailaj <shammishailaj@gmail.com>
2018-09-27 17:50:49 +0530

    changing packaging structure

--- string/explode.go
+++
@@ -1,9 +0,0 @@
-package string
-
-func Explode(delimiter, text string) []string {
-	if len(delimiter) > len(text) {
-		return strings.Split(delimiter, text)
-	} else {
-		return strings.Split(text, delimiter)
-	}
-}
